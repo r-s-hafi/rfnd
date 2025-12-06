9da53d77f6471b335e700524b8b0bcc08dfd4c59
r-s-hafi <shafi.ryana@gmail.com>
2025-12-05 21:07:20 -0600

    Added html database to store plot html for each tag

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import sqlite3
 
 import plotly.express as p
 
-from database import initialize_db, plot_tag_data
+from database import initialize_data_db, plot_tag_data, initialize_html_db, store_html, render_html
 
 #create the fastapi instance, Jinja2 templates to return HTML, and initialize database
 app = FastAPI()
 templates = Jinja2Templates('./templates')
-con = sqlite3.connect("process_data.db")
+con_data = sqlite3.connect("process_data.db")
+con_html = sqlite3.connect("plot_html.db")
 #counter for number of plots
 plot_count = 0
 #list of tags currently plotted
@@ -20,7 +21,8 @@ current_plots = []
 @app.get("/")
 async def initialize(request: Request) -> HTMLResponse:
    #initialize database and populate with the data from the csv
-   initialize_db(con)
+   initialize_data_db(con_data)
+   initialize_html_db(con_html)
    #return homepage "index.html"
    return templates.TemplateResponse(request, "index.html")
 
@@ -28,9 +30,11 @@ async def initialize(request: Request) -> HTMLResponse:
 async def get_tag_id(request: Request, tag_id: str = Form()) -> HTMLResponse:
    #declare plot count and current plots as global variables
    global plot_count, current_plots
+   tag_id = tag_id.upper()
    try:
       #call plot data to collect tag data and return html graph
-      plot_html = plot_tag_data(con, tag_id)
+      plot_html = plot_tag_data(con_data, tag_id)
+      store_html(con_html, tag_id, plot_html)
    
    except Exception as e:
       return HTMLResponse(f"""
@@ -39,19 +43,16 @@ async def get_tag_id(request: Request, tag_id: str = Form()) -> HTMLResponse:
                         """)
    
    #check for repeat plots
-   print(current_plots)
    if tag_id in current_plots:
-      return HTMLResponse(f"""
-                        <h1>Plot already exists for tag {tag_id}</h1>
-                        <p>Please enter a different tag ID</p>
-                        """)
+      print(f"Plot already exists for tag {tag_id}")
    
    else:
       #if try block runs, add plot count to html response
       current_plots.append(tag_id)
       plot_count += 1
-
-   return HTMLResponse(f"""
-                        <h1>Plotting data for tag {tag_id}</h1>
-                        <div id="plot">{plot_html}</div>
+      rendered_html = render_html(tag_id, con_html, current_plots)
+      return HTMLResponse(f"""
+                        <div id="plot-area" hx-swap-oob="true">
+                           {rendered_html}
+                        </div>
                         """)
--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from fastapi.responses import HTMLResponse
 import plotly.express as px
 import plotly.io as pio
 
-def initialize_db(con: Connection) -> None:
+#creates process data database and populates with data from data.csv
+def initialize_data_db(con: Connection) -> None:
     try:
         #read the data.csv file into a pandas dataframe
         df = pd.read_csv("data.csv")
@@ -17,16 +18,51 @@ def initialize_db(con: Connection) -> None:
             cur = con.cursor()
             res = cur.execute("SELECT * FROM process_data")
             print(res.fetchall())
-
+    
     except Exception as e:
         print(f"Unable to import data: {e}")
 
+#creates html database
+def initialize_html_db(con: Connection) -> None:
+    try:
+        #create a new table in the database
+        con.execute("CREATE TABLE IF NOT EXISTS plot_html (tag_id TEXT, html TEXT)")
+    
+    except Exception as e:
+        print(f"Unable to initialize HTML database: {e}")
 
+#plots data for given tag id and returns html
 def plot_tag_data(con: Connection, tag_id: str) -> HTMLResponse:
     tag_id = tag_id.upper()
     df = pd.read_sql(f"SELECT Time, {tag_id} FROM process_data", con)
     fig = px.line(df, x="Time", y=tag_id, title=f"{tag_id}", labels={'Time': 'Time', tag_id: 'Value'})
-    fig.show()
     plot_html = pio.to_html(fig)     
-    return plot_html      
+    return plot_html
 
+#stores html for given tag id
+def store_html(con: Connection, tag_id: str, html: str) -> None:
+    try:
+        wrapped_html = f'<div id="plot">{html}</div>'
+        with con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO plot_html VALUES (?, ?)", (tag_id, wrapped_html))
+    except Exception as e:
+        print(f"Unable to write html to HTML database: {e}")
+
+def render_html(tag_id: str, con: Connection, current_plots: list) -> str:
+    rendered_html = ""
+    for tag_id in current_plots:
+        try:
+            with con:
+                cur = con.cursor()
+                cur.execute(f"SELECT html FROM plot_html WHERE tag_id = '{tag_id}'")
+                html = cur.fetchone()
+                if html:
+                    html = html[0]
+                    rendered_html += html
+                    print(f"successfully rendered html for tag {tag_id}")
+        
+        except Exception as e:
+            print(f"Unable to render html for tag {tag_id}: {e}")
+            continue
+    return rendered_html
